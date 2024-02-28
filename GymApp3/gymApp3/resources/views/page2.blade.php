247682a20c53372145c3242e0be6a4249bcee54b
TkyoZero <103830860+TkyoZero@users.noreply.github.com>
2024-02-28 11:40:43 +0100

    add svg

--- GymApp3/gymApp3/resources/views/page2.blade.php
+++ GymApp3/gymApp3/resources/views/page2.blade.php
@@ -16,25 +16,53 @@
                     </p>
 
                     <div class="exercise-gallery mt-4">
-                        <div class="arm">
+                        <div class="uebung arm">
                             <a href="{{ route('arm') }}" class="exercise-item" data-description="Beschreibung für Armübungen">
-                                <img src="arm.jpg" alt="Armübungen" style="height:200px, width:200px";>
+                                <img src="arm.jpg" alt="Armübungen">
                             </a>
+                            <h2>Arm</h2>
+                            <svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" viewBox="0 0 30 30">
+                              <g id="Ellipse_8" data-name="Ellipse 8" fill="#3772ff" stroke="#3772ff" stroke-width="1">
+                                <circle cx="15" cy="15" r="15" stroke="none"/>
+                                <circle cx="15" cy="15" r="14.5" fill="none"/>
+                              </g>
+                            </svg>
                         </div>
-                        <div class="bauch">
+                        <div class="uebung bauch">
                             <a href="{{ route('bauch') }}" class="exercise-item" data-description="Beschreibung für Bauchübungen">
-                                <img src="bauch.jpg" alt="Bauchübungen" style="height:200px, width:200px";>
+                                <img src="bauch.jpg" alt="Bauchübungen">
                             </a>
+                            <h2>Bauch</h2>
+                            <svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" viewBox="0 0 30 30">
+                              <g id="Ellipse_8" data-name="Ellipse 8" fill="#3772ff" stroke="#3772ff" stroke-width="1">
+                                <circle cx="15" cy="15" r="15" stroke="none"/>
+                                <circle cx="15" cy="15" r="14.5" fill="none"/>
+                              </g>
+                            </svg>
                         </div>
-                        <div class="beine">
+                        <div class="uebung beine">
                             <a href="{{ route('beine') }}" class="exercise-item" data-description="Beschreibung für Beinübungen">
-                                <img src="leg.jpg" alt="Beinübungen" style="height:200px, width:200px";>
+                                <img src="leg.jpg" alt="Beinübungen">
                             </a>
+                            <h2>Bein</h2>
+                            <svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" viewBox="0 0 30 30">
+                              <g id="Ellipse_8" data-name="Ellipse 8" fill="#3772ff" stroke="#3772ff" stroke-width="1">
+                                <circle cx="15" cy="15" r="15" stroke="none"/>
+                                <circle cx="15" cy="15" r="14.5" fill="none"/>
+                              </g>
+                            </svg>
                         </div>
-                        <div class="ruecken">
+                        <div class="uebung ruecken">
                             <a href="{{ route('ruecken') }}" class="exercise-item" data-description="Beschreibung für Rückenübungen">
-                                <img src="rücken.jpg" alt="Rückenübungen" style="height:200px, width:200px";>
+                                <img src="rücken.jpg" alt="Rückenübungen">
                             </a>
+                            <h2>Rücken</h2>
+                            <svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" viewBox="0 0 30 30">
+                              <g id="Ellipse_8" data-name="Ellipse 8" fill="#3772ff" stroke="#3772ff" stroke-width="1">
+                                <circle cx="15" cy="15" r="15" stroke="none"/>
+                                <circle cx="15" cy="15" r="14.5" fill="none"/>
+                              </g>
+                            </svg>
                         </div>
                     </div>
                 </div>
@@ -61,8 +89,19 @@
 </x-app-layout>
 
 <style>
+    
+    /* google font nunito */
+    @import url('https://fonts.googleapis.com/css2?family=Nunito:ital,wght@0,200..1000;1,200..1000&display=swap');
+
+    h2 {
+        font-family: "Nunito", sans-serif;
+        font-optical-sizing: auto;
+        font-weight: 500;
+        font-style: normal;
+        font-size: 30px
+    }
     .exercise-gallery {
-        display: flex;
+        display: block;
         justify-content: center;
         align-items: center;
         gap: 20px;
@@ -76,18 +115,32 @@
         text-decoration: none;
     }
 
-    .exercise-item img {
-        max-width: 150px;
-        height: auto;
-        border-radius: 8px;
-    }
-
+    
     .exercise-item.hovered {
         transform: rotateY(180deg);
     }
-
+    
+    
+    /* uebung */
     .exercise-gallery > * {
         border: 2px solid #3772FF;
         border-radius: 10px;
+    }
+    
+    .uebung {
+        display: flex;
+        align-items: center;
+        /* justify-content: space-around; */
+        gap: 10px;
+        height: 130px;
+        width: auto;
+    }
+    
+    .exercise-item img {
+        width: 100px;
+        height: 100px;
+        object-fit: cover;
+        object-position: left 20%;
+        border-radius: 8px;
     }
 </style>
