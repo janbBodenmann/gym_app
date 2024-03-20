35eb54b40ef39171eed9f01f130e36ba65e5ae44
RobinHood0101 <robinkammer@proton.me>
2024-03-20 10:23:30 +0100

    Fixed Bug in application-logo
    Updated path in application-logo.blade.php component to utilize the asset helper for accessing the public file logo.png.

--- GymApp3/gymApp3/resources/views/components/application-logo.blade.php
+++ GymApp3/gymApp3/resources/views/components/application-logo.blade.php
@@ -1,3 +1,6 @@
+<?php
+$imagePath = asset('logo.png');
+?>
 <svg viewBox="-70 60 450 300" xmlns="http://www.w3.org/2000/svg" {{ $attributes }}>
-    <image xlink:href="logo.png" width="100%" height="140%" />
+    <image xlink:href="{{$imagePath}}" width="100%" height="140%" />
 </svg>
